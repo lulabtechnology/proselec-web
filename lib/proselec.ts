4e89c805bd48cd99e9c2520d5fbb0db55a48b33b
lulabtechnology <131550504+lulabtechnology@users.noreply.github.com>
2026-01-03 14:40:16 -0600

    Update proselec.ts

--- lib/proselec.ts
+++ lib/proselec.ts
@@ -1,9 +1,5 @@
 // lib/proselec.ts
-export { site, contact } from "@/content/site";
+import { site, contact } from "@/content/site";
 
-// Aliases para tu código viejo (CONTACT/COMPANY)
-export { contact as CONTACT } from "@/content/site";
-export { site as COMPANY } from "@/content/site";
-
-// Proyectos (los definimos abajo)
-export { PROJECTS } from "@/content/projects";
+export const COMPANY = site;
+export const CONTACT = contact;
