3788d3f4beb6a6033b82b9ddb9569056ef1a27cb
lulabtechnology <131550504+lulabtechnology@users.noreply.github.com>
2026-01-03 14:58:02 -0600

    Update proselec.ts

--- lib/proselec.ts
+++ lib/proselec.ts
@@ -1,5 +1,7 @@
-// lib/proselec.ts
+// /lib/proselec.ts
 import { site, contact } from "@/content/site";
 
 export const COMPANY = site;
 export const CONTACT = contact;
+
+export { site, contact };
